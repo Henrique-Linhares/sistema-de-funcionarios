c71ec38758994a6ee3aeda960c1969b007f68979
Alice <alice.prados07@gmail.com>
2025-09-18 19:22:47 -0300

    Foi colocado os métodos getters e setters e construtores

--- sistemaDeFuncionarios/src/entity/Gerente.java
+++ sistemaDeFuncionarios/src/entity/Gerente.java
@@ -1,5 +1,49 @@
 package entity;
 
-public class Gerente {
+public class Gerente extends FuncionarioCLT {
+    private String departamento;
+    private int quantidadeDeSubordinados;
+    private boolean salaPropria;
+
+    //Construtor vazio 
+    public Gerente() {
+    }
+
+    //Construtor
+    public Gerente(String departamento, int quantidadeDeSubordinados, boolean salaPropria) {
+        this.departamento = departamento;
+        this.quantidadeDeSubordinados = quantidadeDeSubordinados;
+        this.salaPropria = salaPropria;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getQuantidadeDeSubordinados() {
+        return quantidadeDeSubordinados;
+    }
+
+    public void setQuantidadeDeSubordinados(int quantidadeDeSubordinados) {
+        this.quantidadeDeSubordinados = quantidadeDeSubordinados;
+    }
+
+    public boolean isSalaPropria() {
+        return salaPropria;
+    }
+
+    public void setSalaPropria(boolean salaPropria) {
+        this.salaPropria = salaPropria;
+    }
+
     
+
+    
+
+    
+
 }
