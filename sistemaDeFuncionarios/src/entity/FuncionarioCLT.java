725fab1965fc06f3321f2fe1a7d95772dc3bab42
Leo5300 <lleonardoou89325@gmail.com>
2025-09-18 22:53:35 -0300

    Terceiro commit

--- sistemaDeFuncionarios/src/entity/FuncionarioCLT.java
+++ sistemaDeFuncionarios/src/entity/FuncionarioCLT.java
@@ -1,44 +1,85 @@
 package entity;
 
 public class FuncionarioCLT extends Funcionario {
-    private Double ValeTransporte;
-    private Double ValeAlimentacao;
-    private Boolean PlanoDeSaude;
-   
-    //Construtor vazio
+
+    private Double valeTransporte;
+    private Double valeAlimentacao;
+    private Boolean planoDeSaude;
+
+    // Construtor vazio
     public FuncionarioCLT() {}
 
-    //Constutor
-    public FuncionarioCLT(String nome, int feriasDias, Boolean ferias, String genero, Double salarioBase, String cpf, String registro, 
-    Double valeTransporte, Double valeAlimentacao, Boolean planoDeSaude) {
-
-    super(nome, feriasDias, ferias, genero, salarioBase, cpf, registro );
-    this.ValeTransporte = valeTransporte;
-    this.ValeAlimentacao = valeAlimentacao;
-    this.PlanoDeSaude = planoDeSaude;
-
+    // Construtor parametrizado (chamando super + atributos da subclasse)
+    public FuncionarioCLT(
+        String nome, 
+        int feriasDias, 
+        Boolean ferias, 
+        String genero, 
+        Double salarioBase, 
+        String cpf, 
+        String registro, 
+        Double valeTransporte, 
+        Double valeAlimentacao, 
+        Boolean planoDeSaude
+    ) {
+        super(nome, feriasDias, ferias, genero, salarioBase, cpf, registro);
+        this.valeTransporte = valeTransporte;
+        this.valeAlimentacao = valeAlimentacao;
+        this.planoDeSaude = planoDeSaude;
     }
 
-    //getters e setters
+    // Sobrescrevendo calcularSalarioLiquido
+    @Override
+    public Double calcularSalarioLiquido(Double salarioBase) {
+        Double liquido = super.calcularSalarioLiquido(salarioBase);
+        return liquido + this.valeTransporte + this.valeAlimentacao;
+    }
+
+    // Sobrescrevendo listarDados (toString)
+    @Override
+    public String toString() {
+        return super.toString() + 
+               ", Vale Transporte=" + valeTransporte + 
+               ", Vale Alimentação=" + valeAlimentacao + 
+               ", Plano de Saúde=" + (planoDeSaude ? "Ativo" : "Inativo");
+    }
+
+    // Sobrescrevendo atualizarSalario
+    @Override
+    public void atualizaSalario(Double valor) {
+        setSalarioBase(getSalarioBase() + valor);
+        System.out.println("Novo salário CLT: " + getSalarioBase());
+    }
+ 
+    // Sobrescrevendo atualizarFerias
+    @Override
+    public void atualizarFerias(int dias, boolean estado) {
+        super.atualizarFerias(dias, estado); // Mantém comportamento do pai
+        System.out.println("Registro atualizado no sistema CLT.");
+    }
+
+    // Getters e Setters
     public Double getValeTransporte() {
-        return ValeTransporte;
+        return valeTransporte;
     }
 
     public void setValeTransporte(Double valeTransporte) {
-        ValeTransporte = valeTransporte;
+        this.valeTransporte = valeTransporte;
     }
+
     public Double getValeAlimentacao() {
-        return ValeAlimentacao;
+        return valeAlimentacao;
     }
+
     public void setValeAlimentacao(Double valeAlimentacao) {
-        ValeAlimentacao = valeAlimentacao;
+        this.valeAlimentacao = valeAlimentacao;
     }
+
     public Boolean getPlanoDeSaude() {
-        return PlanoDeSaude;
+        return planoDeSaude;
     }
+
     public void setPlanoDeSaude(Boolean planoDeSaude) {
-        PlanoDeSaude = planoDeSaude;
+        this.planoDeSaude = planoDeSaude;
     }
-
-
 }
